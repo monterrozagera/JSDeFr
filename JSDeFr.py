7b73ddc45d47045032d58bb658fa3138cac78194
monterrozagera <monterrozagerardo@protonmail.com>
2022-10-17 23:01:52 -0700

    add countInstances openScript and controlFlowReconstruct

--- JSDeFr.py
+++ JSDeFr.py
@@ -49,10 +49,15 @@ class Array_Replace:
         print(f'[*] Secrets array: {new}')
 
 
-    def countIntances(self, to_count):
+    def countIntances(self, regex_to_count) -> int:
         """ Counts the times that the given argument is referenced """
         """ in the script. """
-        pass
+        count = 0
+        lines = self.openScript(self.s_name)
+        count = len(re.findall(regex_to_count, lines))
+
+        return count
+
 
     def hexReplace(self):
         """ Replaces all hex digits to decimals. """
@@ -223,6 +228,21 @@ class Array_Replace:
         
         new.append(front)
         return new
+
+    def controlFlowReconstruct(self):
+        """ let's first assume we can somehow identify objects in a super-obfuscated Javascript file """
+        regex_call = r"[a-zA-Z]*0*_*0x[\da-zA-Z]{1,9}\['[a-zA-Z]{,5}'\]"
+        regex_var = r"[a-zA-Z]*0*_*0x[\da-zA-Z]{1,9}"
+
+        print(f"Intances: {self.countIntances(regex_call)}")
+
+    def openScript(self, name_of_script) -> str:
+        """ replace boilerplate code for opening JS file as string, return as str """
+        full_lines = ''
+        with open(name_of_script, 'r') as wb:
+            full_lines = wb.read()
+
+        return full_lines
 
     def decodeB64(self):
         """ Decodes base64 encoding. """
@@ -611,8 +631,10 @@ if __name__ == '__main__':
     parser.add_argument('-b64', '--base64', action='store_true', help='Decodes base64 items from secrets array', required=False)
     parser.add_argument('-m1', '--mode1', action='store_true', help='Simple 0x021ab2(777) form deobfuscation.')
     parser.add_argument('-m2', '--mode2', action='store_true', help='More advanced 0x021ab2(777, 999) form deobfuscation.')
+    parser.add_argument('-cf', '--controlflow', action='store_true', help='Attempts to reconstruct control flow.')
 
-    secrets = ['\x0a<html>\x0a\x09<head>\x0a\x09\x09<title>Your Files Have Been Encrypted</title>\x0a\x09</head>\x0a\x09<body>\x0a\x09\x09<h1>Your files have been encrypted!</h1>\x0a\x09\x09<p>Please send 0.3 Ethereum to the following address: 0xDEADBEEFDEADBEEFDEADBEEFDEADBEEFDDEADBEEF</p>\x0a\x09\x09<em>For the sake of the project, please DO NOT send any real ethereum to this address!</em>\x0a\x09\x09<br>\x0a\x09\x09<p>Make sure you send this:\x0a\x09\x09<br>\x0a\x09\x09<p><em>', 'encrypt.js', '444853DZYvyf', 'encrypt.elf', 'decrypt.elf', '11812430VPfLUf', '.iv', 'exit', 'log', '9cYppBJ', '1139330EgIpFB', 'encrypt.app', 'execPath', 'hex', '345079NDoWoE', 'path', '718521DqYxMC', '</em></p>\x0a\x09\x09<p>Along with your transaction as a message input. We will soon get in touch</p>\x0a\x09\x09<br>\x0a\x09\x09<h1>Warning: Any tampering with any of the files in this directory will result in permanent loss of the file</h1>\x0a\x09</body>\x0a</html>\x0a', 'NOTICE.html', 'decrypt.js', '2174888qWETkn', 'isFile', 'encrypt.exe', '.encrypted', 'pkg', 'device_key_encrypted.dat', '96AbDBCm', 'aes-192-cbc', 'toString', 'forEach', '-----BEGIN PUBLIC KEY-----\x0aMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoK+jQ8f3O3Ug4GdLEK5X\x0aMixXGi72NMIhU2lEE/BXF3C42J/fjVNYPXvZPzX/Cz3atGBX7D2nK28YntVvAII0\x0adEZMvcmkR5Y4ZYahFWYyU4NcOWU/TEMv2w3IsQ4Eageu+1D+qS9rCIbj619wYCeJ\x0ancpMwYD2b0fGpyHZUEfNLX2Qf8QRZPKRtQx05MUPNjgRO/nnXDjq6xs7hEYZB6Nd\x0afvGjOaggtmY0/7X+wnojkZXyjkbbRRUPiB3/rgXhXsv89T9ioQZO2enK4A6JWn/s\x0abFYyUBmQ+uzhADy3HcQZkfMGGwZho6FFtOQNZv1fPVbAMPxAGm61yG46uWgiqJUV\x0a8QIDAQAB\x0a-----END PUBLIC KEY-----', 'pipe', '88GnaEVx', 'crypto', 'You have already encrypted your files once, are you sure you want to pay more?', '6685oBLNit']
+
+    secrets = ["WuZEl", "MupLS", "DiCKc", "3656148JYoALH", "FvfRm", "JgbJa", "fOmhp", "UoVtf", "WhSnB", "EVPSO", "nMMVY", "ivYDN", "readF", "qcskp", "oOxEM", "get", "PSxUs", "JemAo", "nTNsx", "tALTX", "XARpR", "Dneig", "\\+\\+ ", "JHfTa", "delet", "vONEu", "mKqSs", "gsAdu", "RDXQU", "EfrNK", " (tru", "tivit", "tsASB", "KVeuS", "clear", "zhwoC", "Can n", "xwGAf", "MEMrh", "XLORz", "ntSxf", "DKnFG", "-Fi C", "QjNcN", "NCapK", "FkfQa", "dKxXx", "wvyhs", "stdin", "kRSnz", "WRQeZ", "ZtXig", "teDwk", "oMgLk", "readJ", "IjJXo", "ZUYzZ", "rQWNr", "hhNqN", "CHpBn", "ddeib", "JoMhu", "sleep", "fromC", "quest", "KeMwn", "VQhKV", "GXytB", "IsThe", "bIOSb", "GvScR", "jeEBc", "NILOQ", "tXqSF", '"retu', "iFiAP", "GPDTE", "jMoMS", "KRpOy", "bnlEq", "aXzlr", "LDaEJ", "WlZbl", "bMzJL", "qRcFk", "2221944WcfWls", "Incor", "jXvZy", "ScuyW", "FlesS", "File", "EleSQ", "UwPaG", "ErTNx", "MsGYm", "Could", "NIYPR", "FpthD", "baoGm", "0-9a-", "KIBDy", "jZkGo", "OjLKZ", "ySlmm", "PpCZJ", "965990lGpAtD", "lYVUN", "tOpnG", "VnUwe", "Pleas", "YcdTT", "JcsFY", "utf8", "oXSht", "retur", "close", "jkyJo", "post", "FXBNB", "QPpXI", "lShwq", "BIKWd", "termi", "resol", "ion", "tZANv", "OUUeE", "ZdQTB", "YFvUE", "tXole", "VdtNi", "xwxsC", "YFpgi", "TsSEh", "eInte", "PooHZ", "not e", "llhIp", "PJNeB", "]0;", "readl", "IZunI", "ATzCP", "EPrnh", "gHopi", "mazla", "HXVeA", "qjywI", "e) {}", "hex", "GPpsx", "WVnGN", ")+)+)", "onnec", "setRa", "eCwjA", "BJyFI", "RPlkn", "20826832lioSoD", "cTjNW", "wIjud", "sJStb", "mqFAf", "IVPId", "    ", "veVQj", "fcsDw", "TIsMj", "diLee", "qBGye", "ile", "iLnNo", "fszyV", "MMuxw", "yqPKD", "MMInV", "UoIrl", "xhjMR", "ncoun", "JlrjB", "MWhBv", "cVBvE", "HQmPZ", "WWwLP", "print", "VRcYx", "cwnxJ", "utKDK", "OcbAn", "oogle", "red", "IvphZ", "hOasD", "harCo", "wQUsI", "xEwLU", "PcbrC", "knXTS", "xaFrm", "nwnqW", "ror h", "HxxwH", "bpAgS", "then", "itoaP", "KlZPS", "EIjvo", "eZLvO", "kZVEm", "er a ", "niYTw", "XHwok", "OxRhB", "dFTQb", "exit", "TXUXt", "vfpLa", "Hcvhc", "ctor(", "klGQv", "XGtzJ", "CIgtJ", "hhkKs", "sfwpD", "funct", "GCShF", "AHDoZ", "FRKAO", "ZdOwL", "NGRwl", "UVGIi", "AKmrD", "phTRf", "RBBsW", "y det", "IynRp", "PoDzt", "count", "CzRjC", "hvALN", "JSON", "mPzcc", "dlyef", "An er", "krULr", "xTZvL", "postU", "ZLtTN", "NixkT", "GYqje", "FsWlI", "KHhve", "HBwri", "xist!", "strin", "lZPQV", "hzAgh", "fchTI", "RhHxa", "pfIfw", "WEQRw", "PBFWl", "Conso", "RPSJe", "AMYoA", "PnQMG", "iAskX", "LwAIn", "XdvOk", 'is")(', "hWXKA", "IhZoY", "ggpeQ", "RiWSv", "JbVJB", "VPrZl", "WVNDk", "liyks", "ctory", "yaNYo", "IdLvC", "cREJV", "QplQy", "test", "QYroL", "nUEqx", "fDGVT", "gify", "CEcPs", "vBWIY", "ahYli", "AYrpF", "state", "ocfrF", "zOleW", "paCoQ", "CgEnF", "pVgmC", "chalk", "rCZmW", "outpu", "NKaVC", "aZRbG", "nakmt", "(((.+", "BIrKZ", "tiSGr", "wMode", "phsdA", "bbypV", "SPUAw", "wKqnb", "gLzoD", "naNyd", "stGuy", "QGJtz", "IVjll", "rMOps", "jCQiG", "creat", "57349mJljKT", "PhKvC", "mcwAU", "UieJB", "53627", "vqJZc", "RYmWZ", "check", "JHzPD", "jLOTc", "qHUVh", "uKASB", "zFHjA", "vWSQC", "LndyS", "YdOjv", "ForWi", "ected", "ooCMI", "n (fu", "wINOR", "ZmAFx", "kHQCE", "nctio", "RDqaa", "title", "NwJMJ", "NRuWU", "QPfQf", "zfFoY", "ZUCFp", "gUPxB", "JFXtB", "IUcAf", "kLYmB", "DjDCK", "FileS", "BFdpu", "AWHyR", "ZGoUH", "kQLDQ", "pMZVc", "input", "wOldX", "FZnrh", "recur", "fZHom", "a-zA-", "gger", "rect ", "bCBol", "BLswl", "XTnrZ", "kChYm", "qhsZn", "QFluK", "eZVWe", ".com", "sHldH", "nstru", "ite t", "IpCeI", "oJTis", "cFVdC", "lHRaD", "aYbOK", "RCiKq", "qHjDK", "qnqWL", "XdLhf", "rhtNX", "vQWff", "rtYvA", "    [", "gIPZO", " not ", "LsLdJ", "YZLKx", "oJqfX", "eDire", "RkzVY", "YDQzI", "pWTeD", "YyWtM", "www.g", "wnfbo", "yrbtV", "MTRfT", " to f", "pylon", "fHklk", "mvXaq", "cWMCI", "mCsfn", "lFREs", "ing", "tqoDT", "rn th", "sive", "tGvrI", "DGthq", "cFFKw", "HYKow", "jpGPZ", "o JSO", "zhAth", "JahCp", "njuvD", "YMZAb", "BJvyj", "Riojo", "veDSo", "EznfN", "txDte", "fBkkC", "IkSGf", "QNeDZ", "\\( *\\", "xHRKg", "QcrpM", "CsuhJ", " an e", "vCaSv", "stdou", "ile!", "HwTjA", "sODAW", "PdUkQ", "tlgYW", "XMHHY", "FMPQc", "jvEet", "qkRXg", "YPHQv", "dwrlm", "lcgHZ", "jVsKt", "nBmFb", "zIBJY", "key!", "terva", "yZiLm", "MNeqA", "QxMaF", "CozvL", "ZeWtF", "nPbny", "searc", "ructo", "UjbGY", "tion ", "aHdfR", "BZQOk", "as oc", "YVmfn", "OiutT", "BHjly", "xtNsa", "$]*)", "jMnmT", "apply", "SZylE", "rror!", "WuoyK", "dXKRj", "lengt", "ZwZLh", "tered", "data", "wgFxq", "BZhJZ", "XGQUW", "NXMSe", "PHsOG", "CWWBq", "File ", "tNNuU", "EfMRK", "NjLVG", "parse", "does ", "ForEx", "SON", "iEGvC", "53470Fdhzet", "expor", "Sexie", "cyByw", "DXNSP", "NVCKi", "CfshG", "ot wr", "init", "{}.co", "SYwpk", "eUKGH", "once", "await", "HgwXN", "rotVD", "AcMGl", "actio", "zBDSz", "Print", "XkTaq", "RMpqC", "oiYjk", "KRCMY", "nNlCC", "cured", "MFHgv", "MkwGj", "n() ", "IArMQ", "jdgGQ", "setIn", "PKWci", "xYafA", "OJNBR", "MWtVe", "jLSFR", "oQDsh", "GwxrF", "PDRwt", "YlwCM", "aPOvF", "JBcBi", "LJqFX", "wdpqq", "txdTF", "isFil", "XAeST", "581", "RmTlO", "qctmd", "TbuXC", "eFvqe", "blkDg", "oqSPD", "BeVYH", "loaNi", "VVhqQ", "nvWdB", "dns", "IKnow", "PIXyn", "Qeoat", "dfuIg", "YWSRl", "YdRqW", "FrVFc", "SoZTh", "ync", "jNyhW", "MBAsN", "kMdRK", "eYEJW", "kRWmJ", "WJCNZ", "zA-Z_", "VHaoP", "DPxos", "oKrqC", "bYbtA", "Vnpye", "const", "debu", "qOiMv", "*(?:[", "vqeii", "jZOqt", "XyYAo", "write", "vCfaj", "jCdYh", "TKCMe", "CQYGq", "yuHqY", "OodlL", " func", "DtPuQ", "BoqSP", "rface", "mazJd", "while", "VPwHT", "XAdUA", "FHhjS", "1652860WxdHHM", "qEZdW", "getUR", "TPnIg", "VIAyn", "GzsoA", "miCGq", "HCIDU", "umoCs", "NPkHE", "bCXQx", "SgdCP", "e ent", "AqpNd", "CIjdy", "mkdir", "Sync", "SMqAB", "ctoNu", "WgoVW", "rFpVG", "ROnWd", "sSync", "kxoYz", "cGNEq", "GBBji", "toStr", "ombtl", "SNMaG", "idTUq", "EtLxu", "ziIms", "yJtXt", "YqgFs", "14yBvOYi", "EvNzP", "gnUTs", "hplVN", "aEmsd", "Lsixx", "lJzUz", "hbMNn", "MJOif", "hgGkU", "YUbqx", "gYjeP", "FnNYw", "SRGPq", "FVdzW", "chain", "eFile", "BTzZW", "Vsxgn", "SSBYB", "sMbZU", "LFNat", "has e", "LKYlV", "DDwqq", "sWQVf", "oOMBP", "No Wi", "RXfgM", "jlQge", "EduTL", "call", "yZnwp", "gBYZc", "yFOdB", "soTxH", "sotHJ", "PgKOs", "log", "xCqlN", "XzwqX", "SztkI", "MeOgQ", "Objec", "JeHCr", "txmGL", "KZsBI", "pMCZv", "bHJdc", "SOFCd", "ine", "CVpMA", "QPSVm", "GNUOK", "rOMnH", "ion *", "Ikmtt", "axios", "XXjiT", "nal", "exist", "rphMy", "txStz", "xMZaQ", "KYTSM", "pVxPk", "TwhZM", "ZyvVZ", "Z_$][", "ozuXP", "JoxMV", "mnNoS", "JrTmV", "HjAJE", "nSCaB", "sslTx", "iSPxS", "Sqwjw", "mtSja", "gEQsc", "ygMOl", "TKnnu", "TIlmN", "sUhla", "mOKmJ", "jISkE"]
     args = parser.parse_args()
     init(autoreset=True) # Colorama
 
@@ -633,6 +655,10 @@ if __name__ == '__main__':
         ArrayDeobfs = mode_1(regex2, js_script, new_js_script, magic_number, secrets, hex_translate=True, base64_decode=True)
     elif args.mode2:
         ArrayDeobfs = mode_2(regex, js_script, new_js_script, magic_number, secrets, hex_translate=True)
+    elif args.controlflow:
+        ArrayDeobfs = Array_Replace(regex, js_script, new_js_script, secrets, hex_translate=False)
+        ArrayDeobfs.controlFlowReconstruct()
+        sys.exit(0)
     else:
         print("[!] Please choose a mode [1,2] ex. -m1, -m2")
 
